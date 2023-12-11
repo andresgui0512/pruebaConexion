6ef71f2cc084e34fcee70f0ee2ee4401274cdb7f
shinigami0512 <aguillen@colexio-karbo.com>
2023-12-11 18:56:42 +0100

    commit

---
+++ src/main/java/com/mycompany/pruebaconexion/PruebaConexion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaconexion;
+
+/**
+ *
+ * @author Andrés Guillén
+ */
+public class PruebaConexion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
